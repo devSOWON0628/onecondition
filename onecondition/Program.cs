0852071f65f1b1a373cb8b91c8a5cb166e3d7b56
소원 박 <s2018s29@e-mirim.hs.kr>
2020-05-06 18:08:12 +0900

    중첩 조건문을 논리연산자로 변경

--- onecondition/Program.cs
+++ onecondition/Program.cs
@@ -10,6 +10,14 @@ namespace onecondition
     {
         static void Main(string[] args)
         {
+            Console.Write("입력 : ");
+            int x = int.Parse(Console.ReadLine());
+
+            if(x > 10 &&  x < 20)
+            {
+                Console.WriteLine("조건에 맞습니다.");
+            }
+            
         }
     }
 }
